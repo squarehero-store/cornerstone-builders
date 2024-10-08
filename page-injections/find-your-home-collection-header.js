ea1ba8cc580f67aeee594f6c1d29b9e8f427a04e
houghtoncreative <info@houghtoncreative.co.nz>
2024-10-09 11:54:20 +1300

    sefsf

---
+++ page-injections/find-your-home-collection-header.js
@@ -0,0 +1,6 @@
+// ===================================================================
+//   SquareHero Cornerstone Builders: Find Your Home -  Header Script
+// ===================================================================
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.classList.add('property-listings');
+});
